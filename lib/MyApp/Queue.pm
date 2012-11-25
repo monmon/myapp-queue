42b0e260b9a4e99da8f4ce829d6c10592e64b918
monmon <lesamoureuses@gmail.com>
2012-11-25 17:23:46 +0900

    initial commit

---
+++ lib/MyApp/Queue.pm
@@ -0,0 +1,32 @@
+package MyApp::Queue;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MyApp::Queue -
+
+=head1 SYNOPSIS
+
+  use MyApp::Queue;
+
+=head1 DESCRIPTION
+
+MyApp::Queue is
+
+=head1 AUTHOR
+
+monmon E<lt>noE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
